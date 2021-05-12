9d0813d3aee839f591d0dca905a62617546ad9b2
Hermine Grigoryan <35569024+HermineGrigoryan@users.noreply.github.com>
2021-05-12 13:08:22 +0400

    updated setup

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
-I got it, thank you. I put this in my setup.sh:
-
 mkdir -p ~/.streamlit/
 
-echo "[theme]
+echo "\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
+[theme]
 primaryColor = ‘#84a3a7’
 backgroundColor = ‘#EFEDE8’
 secondaryBackgroundColor = ‘#fafafa’
 textColor= ‘#424242’
 font = ‘sans serif’
-[server]
-headless = true
-port = $PORT
-enableCORS = false
-" > ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml 
